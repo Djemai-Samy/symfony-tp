4087f2eee114412900641f73c490dae18046f603
Djemai Samy <samy.djemai93@gmail.com>
2024-10-23 16:51:44 +0200

    Correction du TP: TodoList

---
+++ src/Controller/TachesController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ListTaches;
+use App\Entity\Tache;
+use App\Repository\ListTachesRepository;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TachesController extends AbstractController
+{
+    #[Route("/taches", name: "taches", methods: ["GET"])]
+    function taches(ListTachesRepository $repo)
+    {
+        // Récuperer toutes les liste de la DB
+        $listeTache = $repo->findAll();
+        // Les envoyer a la vue
+        return $this->render('taches.html.twig', ['listTache' => $listeTache]);
+    }
+
+    #[Route("/taches/create", name: "taches.create", methods: ["POST"])]
+    function createListeTaches(Request $req, ListTachesRepository $repo)
+    {
+
+        // Récuperer les donnée de la requete
+        $nomListe = $req->request->get('nom');
+
+        $nouvelleListeTaches = new ListTaches();
+        $nouvelleListeTaches->setTitle($nomListe);
+        $nouvelleListeTaches->setDate(new DateTime());
+        $repo->sauvegarder($nouvelleListeTaches, true);
+
+        return $this->redirectToRoute("taches");
+    }
+
+    #[Route("/tache/create/{list_id}", name: "tache.create", methods: ["POST"])]
+    function createTache($list_id, ListTachesRepository $repo, Request $req)
+    {
+        $listTache = $repo->find($list_id);
+
+        if (!$listTache) {
+            $this->redirectToRoute('taches', ["message" => "La liste n'existe pas"]);
+        }
+
+        // Créer la tache
+        $nouvelTache = new Tache();
+        // Nommer la tache
+        $nouvelTache->setTitle($req->request->get('titre'));
+
+        // Ajouter la tache a la liste
+        $listTache->ajouteTache($nouvelTache);
+        // Enregistrer la liste
+        $repo->sauvegarder($listTache, true);
+
+        return $this->redirectToRoute('taches');
+    }
+}
+
+// Entity: Representation De la table de la DB
+// Repository:Méthodes qui Permettent d'interagir avec la BD
---
+++ src/Entity/Tache.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TacheRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TacheRepository::class)]
+class Tache
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?bool $isFinished = false;
+
+    #[ORM\ManyToOne(targetEntity: ListTaches::class, inversedBy: "taches")]
+    private $listTache;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIsFinished()
+    {
+        return $this->isFinished;
+    }
+
+    public function setIsFinished($isFinished)
+    {
+        $this->isFinished = $isFinished;
+
+        return $this;
+    }
+
+
+    public function getListTache()
+    {
+        return $this->listTache;
+    }
+
+    public function setListTache($listTache)
+    {
+        $this->listTache = $listTache;
+
+        return $this;
+    }
+}
