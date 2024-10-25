364c2c907b69ef455adbb4ecab7baa646aaf6c89
Djemai Samy <samy.djemai93@gmail.com>
2024-10-25 16:43:41 +0200

    Fin jour 5

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -2,27 +2,92 @@
 
 namespace App\Controller;
 
+use App\Form\InscriptionForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InscriptionController extends AbstractController
 {
-    #[Route("/inscription", name: "inscription")]
-    function inscription()
+    #[Route("/inscription", name: "inscription", methods: ['GET', 'POST'])]
+    function inscription(Request $req)
     {
-        $formulaire = $this->createFormBuilder();
-        $formulaire->setMethod('POST');
-        $formulaire->setAttributes(['class' => "MonID"]);
-        $formulaire
-            ->add(
-                'nom',
-                TextType::class,
-                ["label" => "Nom de famille", 'attr' => ['placeholder' => "Entrez votre nom"]]
-            )
-            ->add('prenom', TextType::class, ['attr' => ['placeholder' => "Entrez votre prénom"]]);
+        // Instancier un objet à lier avec le formulaire
+        $user = new User();
 
-        $inscriptionForm = $formulaire->getForm();
-        return $this->render('inscription.html.twig', ['formulaire' => $inscriptionForm]);
+        // Création du formulaire a partir de la classe InscriptionForm 
+        //en le liant avec l'objet
+        $inscriptionForm = $this->createForm(InscriptionForm::class, $user);
+
+        // Récuperer les données du corps de la requête
+        $inscriptionForm->handleRequest($req);
+
+        // Tester si le formulaire est soumis et valide
+        if ($inscriptionForm->isSubmitted() && $inscriptionForm->isValid()) {
+            // L'objet user sera automatiquement iriguer avec les données du formulaire
+            dump($user);
+        }
+
+        // Retourner la vue avec le formulaire
+        return $this->render(
+            'inscription.html.twig',
+            ['formulaire' => $inscriptionForm->createView()]
+        );
+    }
+}
+
+class User
+{
+    private $email;
+    private $nom;
+    private $prenom;
+    private $genre;
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
     }
 }
---
+++ src/Form/InscriptionForm.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class InscriptionForm extends AbstractType
+{
+
+    function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->setMethod('POST');
+        $builder->setAttributes(['class' => "MonID"]);
+        $builder
+            ->add(
+                'email',
+                EmailType::class,
+                ['attr' => ['placeholder' => "Entrez votre email"], "constraints" => [
+                    new Assert\Email(['message' => 'Email invalide!'])
+                ]]
+            )
+            ->add(
+                'nom',
+                TextType::class,
+                [
+                    "label" => "Nom de famille",
+                    "required" => false,
+                    'attr' => ['placeholder' => "Entrez votre nom"],
+                    "constraints" => [
+                        new Assert\NotBlank(["message" => 'Nom obligatoire']),
+                        new Assert\Length(
+                            [
+                                "min" => 2,
+                                "max" => 255,
+                                'minMessage' => "Le nom est trop court",
+                                "maxMessage" => "Le nom est trop long"
+                            ]
+                        )
+                    ]
+                ]
+            )
+            ->add(
+                'prenom',
+                TextType::class,
+                [
+                    'attr' => ['placeholder' => "Entrez votre prénom"],
+                    "constraints" => [
+                        new Assert\NotBlank(["message" => 'Nom obligatoire']),
+                        new Assert\Length(
+                            [
+                                "min" => 2,
+                                "max" => 255,
+                                'minMessage' => "Le nom est trop court",
+                                "maxMessage" => "Le nom est trop long"
+                            ]
+                        )
+                    ]
+                ]
+            )
+            ->add(
+                'genre',
+                ChoiceType::class,
+                ['choices' => ['Masculin' => "m", 'Feminin' => "f"]]
+            )
+            ->add("Envoyer", SubmitType::class, ["attr" => ['class' => "button"]]);
+
+        $builder->get('nom')->setRequired(false);
+        $builder->get('prenom')->setRequired(false);
+    }
+}
